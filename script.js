ee1a80e97eded0799aa71e5380a56b716b89ea8c
Lstrappare <j.m.cisval1@gmail.com>
2024-01-23 00:09:33 -0600

    Iniciando el Reto

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mensaje del usuario
+let mensaje = document.getElementById('mensaje');
+
+// Vocales que vamos a cifrar
+const e = 'enter';
+const i = 'imes';
+const a = 'ai';
+const o = 'ober';
+const u = 'ufat';
+
